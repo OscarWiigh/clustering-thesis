d9e638b6749d1b0491ec76af7e111295fa89d602
Oscar Wiigh <oscar.wiigh@gmail.com>
2020-04-01 10:30:48 +0200

    Simple card added

---
+++ src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Card.css';
+
+class Card extends React.Component {
+  render() {
+    return <div className="cardbox"><p>Hello world!</p>
+    </div>;
+  }
+}
+
+export default Card;
